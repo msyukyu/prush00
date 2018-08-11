cb00aae64e806ffc78ba91a18b66256553f318de
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-11 10:24:23 +0200

    rush01 finished

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 09:20:34 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/11 10:17:53 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/11 10:23:37 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,8 @@ void	rush(int x, int y)
 				ft_putchar('/');
 			else if ((l == 0 && c == x - 1) || (l == y - 1 && c == 0))
 				ft_putchar('\\');
-			else if (l == 0 || l == y - 1)
-				ft_putchar('-');
-			else if (c == 0 || c == x - 1)
-				ft_putchar('|');
+			else if (l == 0 || l == y - 1 || c == 0 || c == x - 1)
+				ft_putchar('*');
 			else
 				ft_putchar(' ');
 			c++;
