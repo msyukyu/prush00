d98ea4ad383c7711fce16392913a463ad216eaae
Damien Abeloos <dabeloos@w-r5-p7.s19.be>
2018-08-11 10:47:44 +0200

    rush04 finished

--- ex00/rush04.c
+++ ex00/rush04.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 09:20:34 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/11 10:02:14 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/11 10:47:17 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void	rush(int x, int y)
 		c = 0;
 		while (c < x)
 		{
-			if ((c == 0 || c == x - 1) && (l == 0 || l == y - 1))
-				ft_putchar('o');
-			else if (l == 0 || l == y - 1)
-				ft_putchar('-');
-			else if (c == 0 || c == x - 1)
-				ft_putchar('|');
+			if (c + l == 0 || c + l == x + y - 2)
+				ft_putchar('A');
+			else if ((c == 0 && l == x - 1) || (c == y - 1 && l == 0))
+				ft_putchar('C');
+			else if (l == 0 || l == y - 1 || c == 0 || c == x - 1)
+				ft_putchar('B');
 			else
 				ft_putchar(' ');
 			c++;
